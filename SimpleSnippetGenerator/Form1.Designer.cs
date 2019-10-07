10cb6233a7b492f457a29018d1a66d7a61929399
Luka Kalinić <44996320+lukakalinic@users.noreply.github.com>
2019-10-07 15:45:43 +0200

    Initial upload

---
+++ SimpleSnippetGenerator/Form1.Designer.cs
@@ -0,0 +1,280 @@
+﻿namespace SimpleSnippetGenerator
+{
+    partial class Form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.inputRichTextBox = new System.Windows.Forms.RichTextBox();
+            this.insertHereLabel = new System.Windows.Forms.Label();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.resetButton = new System.Windows.Forms.Button();
+            this.insertSnippetRadioButton = new System.Windows.Forms.RadioButton();
+            this.surroundWithRadioButton = new System.Windows.Forms.RadioButton();
+            this.fontSizeNumericUpDown = new System.Windows.Forms.NumericUpDown();
+            this.fontLabel = new System.Windows.Forms.Label();
+            this.snippetTypeGroupBox = new System.Windows.Forms.GroupBox();
+            this.snippetDetailsGroupBox = new System.Windows.Forms.GroupBox();
+            this.authorTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.descriptionRichTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.descriptionLabel = new System.Windows.Forms.Label();
+            this.titleTextBox = new System.Windows.Forms.TextBox();
+            this.titleLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.fontSizeNumericUpDown)).BeginInit();
+            this.snippetTypeGroupBox.SuspendLayout();
+            this.snippetDetailsGroupBox.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // inputRichTextBox
+            // 
+            this.inputRichTextBox.AcceptsTab = true;
+            this.inputRichTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.inputRichTextBox.Location = new System.Drawing.Point(16, 32);
+            this.inputRichTextBox.Name = "inputRichTextBox";
+            this.inputRichTextBox.Size = new System.Drawing.Size(410, 397);
+            this.inputRichTextBox.TabIndex = 0;
+            this.inputRichTextBox.Text = "";
+            this.inputRichTextBox.ZoomFactor = 1.2F;
+            // 
+            // insertHereLabel
+            // 
+            this.insertHereLabel.AutoSize = true;
+            this.insertHereLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.insertHereLabel.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.insertHereLabel.Location = new System.Drawing.Point(12, 9);
+            this.insertHereLabel.Name = "insertHereLabel";
+            this.insertHereLabel.Size = new System.Drawing.Size(157, 20);
+            this.insertHereLabel.TabIndex = 1;
+            this.insertHereLabel.Text = "Insert querry here:";
+            // 
+            // saveButton
+            // 
+            this.saveButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.saveButton.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.saveButton.Location = new System.Drawing.Point(505, 398);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(107, 31);
+            this.saveButton.TabIndex = 2;
+            this.saveButton.Text = "SAVE";
+            this.saveButton.UseVisualStyleBackColor = true;
+            this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
+            // 
+            // resetButton
+            // 
+            this.resetButton.BackColor = System.Drawing.Color.Transparent;
+            this.resetButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.resetButton.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.resetButton.Location = new System.Drawing.Point(432, 398);
+            this.resetButton.Name = "resetButton";
+            this.resetButton.Size = new System.Drawing.Size(67, 31);
+            this.resetButton.TabIndex = 3;
+            this.resetButton.Text = "Reset";
+            this.resetButton.UseVisualStyleBackColor = false;
+            this.resetButton.Click += new System.EventHandler(this.clearButton_Click);
+            // 
+            // insertSnippetRadioButton
+            // 
+            this.insertSnippetRadioButton.AutoSize = true;
+            this.insertSnippetRadioButton.Checked = true;
+            this.insertSnippetRadioButton.Location = new System.Drawing.Point(6, 19);
+            this.insertSnippetRadioButton.Name = "insertSnippetRadioButton";
+            this.insertSnippetRadioButton.Size = new System.Drawing.Size(99, 17);
+            this.insertSnippetRadioButton.TabIndex = 4;
+            this.insertSnippetRadioButton.TabStop = true;
+            this.insertSnippetRadioButton.Text = "Insert Snippet...";
+            this.insertSnippetRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // surroundWithRadioButton
+            // 
+            this.surroundWithRadioButton.AutoSize = true;
+            this.surroundWithRadioButton.Location = new System.Drawing.Point(6, 42);
+            this.surroundWithRadioButton.Name = "surroundWithRadioButton";
+            this.surroundWithRadioButton.Size = new System.Drawing.Size(93, 17);
+            this.surroundWithRadioButton.TabIndex = 5;
+            this.surroundWithRadioButton.Text = "Surround With";
+            this.surroundWithRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // fontSizeNumericUpDown
+            // 
+            this.fontSizeNumericUpDown.Location = new System.Drawing.Point(432, 48);
+            this.fontSizeNumericUpDown.Maximum = new decimal(new int[] {
+            25,
+            0,
+            0,
+            0});
+            this.fontSizeNumericUpDown.Minimum = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            this.fontSizeNumericUpDown.Name = "fontSizeNumericUpDown";
+            this.fontSizeNumericUpDown.Size = new System.Drawing.Size(56, 20);
+            this.fontSizeNumericUpDown.TabIndex = 6;
+            this.fontSizeNumericUpDown.Value = new decimal(new int[] {
+            8,
+            0,
+            0,
+            0});
+            this.fontSizeNumericUpDown.ValueChanged += new System.EventHandler(this.fontSizeNumericUpDown_ValueChanged);
+            // 
+            // fontLabel
+            // 
+            this.fontLabel.AutoSize = true;
+            this.fontLabel.Location = new System.Drawing.Point(429, 32);
+            this.fontLabel.Name = "fontLabel";
+            this.fontLabel.Size = new System.Drawing.Size(49, 13);
+            this.fontLabel.TabIndex = 7;
+            this.fontLabel.Text = "Font size";
+            // 
+            // snippetTypeGroupBox
+            // 
+            this.snippetTypeGroupBox.Controls.Add(this.insertSnippetRadioButton);
+            this.snippetTypeGroupBox.Controls.Add(this.surroundWithRadioButton);
+            this.snippetTypeGroupBox.Location = new System.Drawing.Point(494, 32);
+            this.snippetTypeGroupBox.Name = "snippetTypeGroupBox";
+            this.snippetTypeGroupBox.Size = new System.Drawing.Size(118, 67);
+            this.snippetTypeGroupBox.TabIndex = 8;
+            this.snippetTypeGroupBox.TabStop = false;
+            this.snippetTypeGroupBox.Text = "Snippet Type:";
+            // 
+            // snippetDetailsGroupBox
+            // 
+            this.snippetDetailsGroupBox.Controls.Add(this.authorTextBox);
+            this.snippetDetailsGroupBox.Controls.Add(this.label1);
+            this.snippetDetailsGroupBox.Controls.Add(this.descriptionRichTextBox1);
+            this.snippetDetailsGroupBox.Controls.Add(this.descriptionLabel);
+            this.snippetDetailsGroupBox.Controls.Add(this.titleTextBox);
+            this.snippetDetailsGroupBox.Controls.Add(this.titleLabel);
+            this.snippetDetailsGroupBox.Location = new System.Drawing.Point(432, 105);
+            this.snippetDetailsGroupBox.Name = "snippetDetailsGroupBox";
+            this.snippetDetailsGroupBox.Size = new System.Drawing.Size(180, 215);
+            this.snippetDetailsGroupBox.TabIndex = 9;
+            this.snippetDetailsGroupBox.TabStop = false;
+            this.snippetDetailsGroupBox.Text = "Snippet Details:";
+            // 
+            // authorTextBox
+            // 
+            this.authorTextBox.Location = new System.Drawing.Point(10, 170);
+            this.authorTextBox.MaxLength = 50;
+            this.authorTextBox.Multiline = true;
+            this.authorTextBox.Name = "authorTextBox";
+            this.authorTextBox.Size = new System.Drawing.Size(160, 35);
+            this.authorTextBox.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 155);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Author";
+            // 
+            // descriptionRichTextBox1
+            // 
+            this.descriptionRichTextBox1.Location = new System.Drawing.Point(10, 91);
+            this.descriptionRichTextBox1.MaxLength = 150;
+            this.descriptionRichTextBox1.Name = "descriptionRichTextBox1";
+            this.descriptionRichTextBox1.Size = new System.Drawing.Size(160, 62);
+            this.descriptionRichTextBox1.TabIndex = 3;
+            this.descriptionRichTextBox1.Text = "";
+            // 
+            // descriptionLabel
+            // 
+            this.descriptionLabel.AutoSize = true;
+            this.descriptionLabel.Location = new System.Drawing.Point(7, 75);
+            this.descriptionLabel.Name = "descriptionLabel";
+            this.descriptionLabel.Size = new System.Drawing.Size(60, 13);
+            this.descriptionLabel.TabIndex = 2;
+            this.descriptionLabel.Text = "Description";
+            // 
+            // titleTextBox
+            // 
+            this.titleTextBox.Location = new System.Drawing.Point(10, 37);
+            this.titleTextBox.MaxLength = 50;
+            this.titleTextBox.Multiline = true;
+            this.titleTextBox.Name = "titleTextBox";
+            this.titleTextBox.Size = new System.Drawing.Size(160, 35);
+            this.titleTextBox.TabIndex = 1;
+            // 
+            // titleLabel
+            // 
+            this.titleLabel.AutoSize = true;
+            this.titleLabel.Location = new System.Drawing.Point(7, 20);
+            this.titleLabel.Name = "titleLabel";
+            this.titleLabel.Size = new System.Drawing.Size(27, 13);
+            this.titleLabel.TabIndex = 0;
+            this.titleLabel.Text = "Title";
+            // 
+            // Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(624, 441);
+            this.Controls.Add(this.snippetDetailsGroupBox);
+            this.Controls.Add(this.snippetTypeGroupBox);
+            this.Controls.Add(this.fontLabel);
+            this.Controls.Add(this.fontSizeNumericUpDown);
+            this.Controls.Add(this.resetButton);
+            this.Controls.Add(this.saveButton);
+            this.Controls.Add(this.insertHereLabel);
+            this.Controls.Add(this.inputRichTextBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Simple snippet creator for SQLServer";
+            ((System.ComponentModel.ISupportInitialize)(this.fontSizeNumericUpDown)).EndInit();
+            this.snippetTypeGroupBox.ResumeLayout(false);
+            this.snippetTypeGroupBox.PerformLayout();
+            this.snippetDetailsGroupBox.ResumeLayout(false);
+            this.snippetDetailsGroupBox.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox inputRichTextBox;
+        private System.Windows.Forms.Label insertHereLabel;
+        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.Button resetButton;
+        private System.Windows.Forms.RadioButton insertSnippetRadioButton;
+        private System.Windows.Forms.RadioButton surroundWithRadioButton;
+        private System.Windows.Forms.NumericUpDown fontSizeNumericUpDown;
+        private System.Windows.Forms.Label fontLabel;
+        private System.Windows.Forms.GroupBox snippetTypeGroupBox;
+        private System.Windows.Forms.GroupBox snippetDetailsGroupBox;
+        private System.Windows.Forms.TextBox authorTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox descriptionRichTextBox1;
+        private System.Windows.Forms.Label descriptionLabel;
+        private System.Windows.Forms.TextBox titleTextBox;
+        private System.Windows.Forms.Label titleLabel;
+    }
+}
+
---
+++ SimpleSnippetGenerator/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace SimpleSnippetGenerator
+{
+    public partial class Form : System.Windows.Forms.Form
+    {
+        public Form()
+        {
+            InitializeComponent();
+            fontSizeNumericUpDown.Value = Convert.ToDecimal(inputRichTextBox.Font.Size);
+        }
+
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            //Reseting all
+            inputRichTextBox.Clear();
+            authorTextBox.Clear();
+            titleTextBox.Clear();
+            descriptionRichTextBox1.Clear();
+
+            //Setting default values
+            fontSizeNumericUpDown.Value = 8;
+            setFontSize();
+
+            //Setting default radioButtons to be checked
+            insertSnippetRadioButton.Checked = true;
+            surroundWithRadioButton.Checked = false;
+        }
+
+        private void fontSizeNumericUpDown_ValueChanged(object sender, EventArgs e)
+        {
+            setFontSize();
+        }
+
+        private void setFontSize()
+        {
+            float size = Convert.ToSingle(fontSizeNumericUpDown.Value);
+            Font myFont = new Font(inputRichTextBox.Font.Name, size);
+            inputRichTextBox.Font = myFont;
+        }
+
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+            XNamespace xn = "http://schemas.microsoft.com/VisualStudio/2005/CodeSnippet";
+
+            XDocument xdoc = new XDocument(new XDeclaration("1.0", "utf-8", "yes"));
+            xdoc.Add(new XElement(xn + "CodeSnippets", new XAttribute("xmlns", "http://schemas.microsoft.com/VisualStudio/2005/CodeSnippet"),
+                         new XElement("CodeSnippet", new XAttribute("Format", "1.0.0"),
+                         new XElement("Header",
+                             new XElement("Title", "value1"),
+                             new XElement("Shortcuts"),
+                             new XElement("Description", "desc"),
+                             new XElement("Author", "author"),
+                             new XElement("SnippetTypes",
+                                 new XElement("SnippetType", "tip"))),
+                         new XElement("Snippet",
+                             new XElement("Code", new XAttribute("Language", "XML"), "querry")))));
+
+            //Using only for testing
+            string test = xdoc.ToString();
+        }
+    }
+}
