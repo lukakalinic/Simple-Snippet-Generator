86a41d1da37367ad997ba76dc0060e93b481c541
Luka Kalinić <luka.kalinic95@gmail.com>
2019-10-11 15:16:50 +0200

    Azuriran prikaz linije. [BugFix]

--- SimpleSnippetGenerator/Form1.cs
+++ SimpleSnippetGenerator/Form1.cs
@@ -17,7 +17,7 @@ namespace SimpleSnippetGenerator
     {
         #region Fields
         private int maxLineNumberCharLength;
-        const int padding = 2;
+        private const int padding = 2;
         #endregion
 
         public Form()
@@ -53,6 +53,10 @@ namespace SimpleSnippetGenerator
             //Setting default radioButtons to be checked
             expansionSnippetRadioButton.Checked = true;
             surroundWithRadioButton.Checked = false;
+
+            //Reset state of Scintilla to default state
+            maxLineNumberCharLength = scintillaBox.Lines.Count.ToString().Length;
+            scintillaBox.Margins[0].Width = scintillaBox.TextWidth(Style.LineNumber, new string('9', maxLineNumberCharLength + 1)) + padding;
         }
 
         private void saveButton_Click(object sender, EventArgs e)
@@ -84,12 +88,35 @@ namespace SimpleSnippetGenerator
             {
                 showLineNumbers(scintillaBox);
             }
+            else
+            {
+                //Ovo ovde je obavezno jer npr ako je checkBox unchecked,
+                //svaku novu liniju koju unesem, ako br. linija predje 10, onda 
+                //ce desetku da mi ispise malo odsecenu tj nece upasti u marginu kako treba.
+                //Zato uvek azuriram sirinu margine, iako je ne ispisujem.
+                maxLineNumberCharLength = scintillaBox.Lines.Count.ToString().Length;
+            }
         }
 
         private void fontSizeNumericUpDown_ValueChanged(object sender, EventArgs e)
         {
             float size = Convert.ToSingle(fontSizeNumericUpDown.Value);
             InitSyntaxFormatting(size);
+        }
+
+        private void lineNumberCheckBox_CheckedChanged(object sender, EventArgs e)
+        {
+            if (lineNumberCheckBox.Checked)
+            {
+                scintillaBox.Margins[0].Width = scintillaBox.TextWidth(Style.LineNumber, new string('9', maxLineNumberCharLength + 1)) + padding;
+            }
+            else
+            {
+                foreach (var margin in scintillaBox.Margins)
+                {
+                    margin.Width = 0;
+                }
+            }
         }
 
         #endregion
@@ -108,39 +135,22 @@ namespace SimpleSnippetGenerator
             scintillaBox.SetSelectionBackColor(true, IntToColor(0x114D9C));
 
             // Configure the CPP (C#) lexer styles
-            scintillaBox.Styles[Style.Cpp.Default].ForeColor = Color.Silver;
-            scintillaBox.Styles[Style.Cpp.Comment].ForeColor = Color.FromArgb(0, 128, 0); // Green
-            scintillaBox.Styles[Style.Cpp.CommentLine].ForeColor = Color.FromArgb(0, 128, 0); // Green
-            scintillaBox.Styles[Style.Cpp.CommentLineDoc].ForeColor = Color.FromArgb(128, 128, 128); // Gray
-            scintillaBox.Styles[Style.Cpp.Number].ForeColor = Color.Olive;
-            scintillaBox.Styles[Style.Cpp.Word].ForeColor = Color.Blue;
-            scintillaBox.Styles[Style.Cpp.Word2].ForeColor = Color.Blue;
-            scintillaBox.Styles[Style.Cpp.String].ForeColor = Color.FromArgb(163, 21, 21); // Red
-            scintillaBox.Styles[Style.Cpp.Character].ForeColor = Color.FromArgb(163, 21, 21); // Red
-            scintillaBox.Styles[Style.Cpp.Verbatim].ForeColor = Color.FromArgb(163, 21, 21); // Red
-            scintillaBox.Styles[Style.Cpp.StringEol].BackColor = Color.Pink;
-            scintillaBox.Styles[Style.Cpp.Operator].ForeColor = Color.Purple;
-            scintillaBox.Styles[Style.Cpp.Preprocessor].ForeColor = Color.Maroon;
+            scintillaBox.Styles[Style.Sql.Default].ForeColor = Color.Silver;
+            scintillaBox.Styles[Style.Sql.Comment].ForeColor = Color.FromArgb(0, 128, 0); // Green
+            scintillaBox.Styles[Style.Sql.CommentLine].ForeColor = Color.FromArgb(0, 128, 0); // Green
+            scintillaBox.Styles[Style.Sql.CommentLineDoc].ForeColor = Color.FromArgb(128, 128, 128); // Gray
+            scintillaBox.Styles[Style.Sql.Number].ForeColor = Color.Olive;
+            scintillaBox.Styles[Style.Sql.Word].ForeColor = Color.Blue;
+            scintillaBox.Styles[Style.Sql.Word2].ForeColor = Color.Blue;
+            scintillaBox.Styles[Style.Sql.String].ForeColor = Color.FromArgb(163, 21, 21); // Red
+            scintillaBox.Styles[Style.Sql.Character].ForeColor = Color.FromArgb(163, 21, 21); // Red
+            scintillaBox.Styles[Style.Sql.Operator].ForeColor = Color.Purple;
+            
 
-            scintillaBox.Lexer = Lexer.Cpp;
+            scintillaBox.Lexer = Lexer.Sql;
 
             scintillaBox.SetKeywords(0, "class extends implements import interface new case do while else if for in switch throw get set function var try catch finally while with default break continue delete return each const namespace package include use is as instanceof typeof author copy default deprecated eventType example exampleText exception haxe inheritDoc internal link mtasc mxmlc param private return see serial serialData serialField since throws usage version langversion playerversion productversion dynamic private public partial static intrinsic internal native override protected AS3 final super this arguments null Infinity NaN undefined true false abstract as base bool break by byte case catch char checked class const continue decimal default delegate do double descending explicit event extern else enum false finally fixed float for foreach from goto group if implicit in int interface internal into is lock long new null namespace object operator out override orderby params private protected public readonly ref return switch struct sbyte sealed short sizeof stackalloc static string select this throw true try typeof uint ulong unchecked unsafe ushort using var virtual volatile void while where yield");
             scintillaBox.SetKeywords(1, "void Null ArgumentError arguments Array Boolean Class Date DefinitionError Error EvalError Function int Math Namespace Number Object RangeError ReferenceError RegExp SecurityError String SyntaxError TypeError uint XML XMLList Boolean Byte Char DateTime Decimal Double Int16 Int32 Int64 IntPtr SByte Single UInt16 UInt32 UInt64 UIntPtr Void Path File System Windows Forms ScintillaNET");
-        }
-
-        private void lineNumberCheckBox_CheckedChanged(object sender, EventArgs e)
-        {
-            if (lineNumberCheckBox.Checked)
-            {
-                scintillaBox.Margins[0].Width = scintillaBox.TextWidth(Style.LineNumber, new string('9', maxLineNumberCharLength + 1)) + padding;
-            }
-            else
-            {
-                foreach (var margin in scintillaBox.Margins)
-                {
-                    margin.Width = 0;
-                }
-            }
         }
 
         private void showLineNumbers(Scintilla scintillaBox)
